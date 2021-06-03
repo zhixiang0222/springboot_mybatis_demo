4178907d5af070b07c64010c7cea93dba056fb43
zhixiang0222 <zhixiang0222@163.com>
2021-06-03 15:49:17 +0800

    编写了一个测试方法

---
+++ src/main/java/cn/wisefly/springboot_mybatis_demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package cn.wisefly.springboot_mybatis_demo.controller;
+
+
+public class TestController {
+
+
+  public void test() {
+    boolean flag = true;
+    if (flag) {
+      System.out.println("------");
+    }
+  }
+}
